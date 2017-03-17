cd0f78b5fc32200fb82ee131ad934579e59802c0
zhangling867 <957829157@qq.com>
2017-03-17 16:26:13 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+var express=require("C:/Users/z/node_modules/express"),
+app=express(),
+server=require('http').createServer(app),
+io=require('C:/Users/z/node_modules/socket.io').listen(server),
+users=[];
+app.use('/',express.static(__dirname+'/www'));
+server.listen(8080);
+io.on('connection',function(socket){
+	socket.on('login',function(nickname){
+		if(users.indexOf(nickname)>-1){
+			socket.emit('nickExisted');
+		}else{
+			socket.userIndex=users.length;
+			socket.nickname=nickname;
+			users.push(nickname);
+			socket.emit('loginSuccess');
+			io.sockets.emit("system",nickname,users.length,"login")
+		}
+	})
+	socket.on('postMsg',function(msg,color){
+		socket.broadcast.emit('newMsg',socket.nickname,msg,color)
+	})
+	socket.on("img",function(imgData){
+		socket.broadcast.emit("newImg",socket.nickname,imgData)
+	})
+	socket.on("disconnect",function(){
+		users.splice(socket.userIndex,1);
+		socket.broadcast.emit("system",socket.nickname,users.length,'logout')
+	})
+})
